7b73b39e784e94950e74ca99c613ee77b0374508
giogiocamp96 <giorgia.campanile06@gmail.com>
2023-07-31 16:38:44 +0200

    Initial Commit

---
+++ LuxOttica_start/LuxOttica_startApp.swift
@@ -0,0 +1,17 @@
+//
+//  LuxOttica_startApp.swift
+//  LuxOttica_start
+//
+//  Created by Giorgia Campanile on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LuxOttica_startApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
